6464ea633be61fd2f0d932285d84ee99f7b3f68d
AnatoliSS <avshayke@yahoo.com>
2016-03-12 17:23:21 -0500

    A fix

--- cachematrix.R
+++ cachematrix.R
@@ -11,7 +11,7 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setinv <- function(inv) m <<- inv
+  setinv <- function(solve) m <<- solve
   getinv <- function() m
   list(set = set, get = get,
        setinv = setinv,
